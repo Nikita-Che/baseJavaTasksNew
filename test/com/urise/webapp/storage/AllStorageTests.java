3b82975a3631aff188da04c2f1b61421f1050916
homePc <nikitachertkov@gmail.com>
2022-12-02 20:57:50 +0500

    test

--- test/com/urise/webapp/storage/AllStorageTests.java
+++ test/com/urise/webapp/storage/AllStorageTests.java
@@ -16,6 +16,7 @@ import org.junit.platform.suite.api.Suite;
                 XmlPathStorageTest.class,
                 JsonPathStorageTest.class,
                 DataPathStorageTest.class
+
         })
 public class AllStorageTests {
 
