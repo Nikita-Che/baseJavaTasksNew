09fc420798fa63a167ed06057b69041f860314bb
nikita <nikitachertkov@gmail.com>
2022-12-13 20:00:07 +0500

    ACHIEVEMENT, QUALIFICATIONS read

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -86,10 +86,7 @@ public class DataStreamSerializer implements SerializerStrategie {
                     }
                     case ACHIEVEMENT, QUALIFICATIONS -> {
                         List<String> stringList = new ArrayList<>();
-                        int listSize = dis.readInt();
-                        for (int j = 0; j < listSize; j++) {
-                            stringList.add(dis.readUTF());
-                        }
+                        readWithException(dis, () -> stringList.add(dis.readUTF()));
                         sectionMap.put(sectionType, new ListSection(stringList));
                     }
                     case EDUCATION, EXPERIENCE -> {
