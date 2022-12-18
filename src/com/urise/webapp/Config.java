1f9fde6d2f7fede0cb1a8abbba612ce8dc8317ca
nikita <nikitachertkov@gmail.com>
2022-12-18 23:47:16 +0500

    config3

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -24,7 +24,6 @@ public class Config {
             throw new IllegalStateException("Invalid config file! " + PROPS.getAbsolutePath());
         }
     }
-
     public File getStorageDir() {
         return storageDir;
     }
